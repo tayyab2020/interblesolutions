e119bed1e3ca67b69f905bb1ce5a4e5a12fe2a54
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-10-15 18:23:17 +0500

    Updates

---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+    <!-- Author -->
+    <meta name="author" content="Themes Industry">
+    <!-- description -->
+    <meta name="description" content="MegaOne is a highly creative, modern, visually stunning and Bootstrap responsive multipurpose studio and portfolio HTML5 template with 8 ready home page demos.">
+    <!-- keywords -->
+    <meta name="keywords" content="Creative Startup, modern, clean, bootstrap responsive, html5, css3, portfolio, blog, studio, templates, multipurpose, one page, corporate, start-up, studio, branding, designer, freelancer, carousel, parallax, photography, studio, masonry, grid, faq">
+    <!-- Page Title -->
+    <title>Interblesolutions</title>
+    <!-- Favicon -->
+    <link href="" rel="icon">
+    <!-- Bundle -->
+    <link href="vendor/css/bundle.min.css" rel="stylesheet">
+    <!-- Plugin Css -->
+    <link href="creative-startup/css/line-awesome.min.css" rel="stylesheet">
+    <link href="vendor/css/revolution-settings.min.css" rel="stylesheet">
+    <link href="vendor/css/jquery.fancybox.min.css" rel="stylesheet">
+    <link href="vendor/css/owl.carousel.min.css" rel="stylesheet">
+    <link href="vendor/css/cubeportfolio.min.css" rel="stylesheet">
+    <link href="vendor/css/LineIcons.min.css" rel="stylesheet">
+    <link href="creative-startup/css/slick.css" rel="stylesheet">
+    <link href="creative-startup/css/slick-theme.css" rel="stylesheet">
+    <link href="vendor/css/wow.css" rel="stylesheet">
+    <!-- Style Sheet -->
+    <link href="creative-startup/css/blog.css" rel="stylesheet">
+    <link href="creative-startup/css/style.css" rel="stylesheet">
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+<!-- Preloader -->
+<div class="preloader">
+    <div class="center">
+        <div class="loader loader-32">
+            <div class='loader-container'>
+                <div class='ball-wrapper'>
+                    <div class='ball-holder'>
+                        <div class='ball'></div>
+                    </div>
+                    <div class='shadow'></div>
+                </div>
+                <div class='ball-wrapper'>
+                    <div class='ball-holder'>
+                        <div class='ball'></div>
+                    </div>
+                    <div class='shadow'></div>
+                </div>
+                <div class='ball-wrapper'>
+                    <div class='ball-holder'>
+                        <div class='ball'></div>
+                    </div>
+                    <div class='shadow'></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Preloader End -->
+
+<!--Header Start-->
+<header id="home">
+
+    <div class="inner-header">
+        <!--colored-lines-->
+        <div class="color-lines row no-gutters">
+            <div class="col-4 bg-red"></div>
+            <div class="col-4 bg-purple"></div>
+            <div class="col-4 bg-green"></div>
+        </div>
+        <!--upper-nav-->
+        <div class="upper-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <ul class="top-personal-info">
+                            <li><a href="tel:08053545622"><i class="las la-phone"></i> +1 805-354-5622</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 text-right">
+                        <ul class="top-social-links">
+                            <li><a href="#" class="link-holder fb"><i class="lab la-facebook-f"></i></a></li>
+                            <li><a href="#" class="link-holder twit"><i class="lab la-twitter"></i></a></li>
+                            <li><a href="#" class="link-holder link-in"><i class="lab la-linkedin-in"></i></a></li>
+                            <li><a href="#" class="link-holder insta"><i class="lab la-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--main nav-->
+        <div class="main-navigation">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4 col-lg-3">
+                        <a class="navbar-brand simple-nav-logo" href="index-creative-startup.html">
+                            <img src="creative-startup/img/logo.png" alt="logo">
+                        </a>
+                        <a class="navbar-brand fixed-nav-logo" href="index-creative-startup.html">
+                            <img src="creative-startup/img/logo-black.png" alt="logo">
+                        </a>
+                    </div>
+                    <div class="col-8 col-lg-9 simple-navbar">
+                        <nav class="navbar navbar-expand-lg">
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav ml-auto">
+                                    <li class="nav-item active"><a class="nav-link home" href="#">HOME</a></li>
+                                    <li class="nav-item"><a class="nav-link scroll" href="#about-sec">ABOUT</a></li>
+                                    <li class="nav-item"><a class="nav-link scroll" href="#company-portfolio-section">PORTFOLIO</a></li>
+                                    <li class="nav-item"><a class="nav-link scroll" href="#testimonial-sec">CLIENTS</a></li>
+                                    <li class="nav-item"><a class="nav-link scroll" href="#blog-sec">BLOG</a></li>
+                                    <li class="nav-item"><a class="nav-link scroll" href="#contact-sec">CONTACT</a></li>
+                                </ul>
+                            </div>
+                        </nav>
+                        <ul class="top-social-links fixed-nav-links">
+                            <li><a href="#" class="link-holder fb"><i class="lab la-facebook-f"></i></a></li>
+                            <li><a href="#" class="link-holder twit"><i class="lab la-twitter"></i></a></li>
+                            <li><a href="#" class="link-holder link-in"><i class="lab la-linkedin-in"></i></a></li>
+                            <li><a href="#" class="link-holder insta"><i class="lab la-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--toggle btn-->
+        <a href="javascript:void(0)" class="sidemenu_btn" id="sidemenu_toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </a>
+    </div>
+    <!--Side Nav-->
+    <div class="side-menu hidden side-menu-opacity">
+        <div class="bg-overlay"></div>
+        <div class="inner-wrapper">
+            <span class="btn-close" id="btn_sideNavClose"><i></i><i></i></span>
+            <div class="container">
+                <div class="row w-100 side-menu-inner-content">
+                    <div class="col-12 d-flex justify-content-center align-items-center">
+                        <a href="index-creative-startup.html" class="navbar-brand"><img src="creative-startup/img/logo.png" alt="logo"></a>
+                    </div>
+                    <div class="col-12 col-lg-8">
+                        <nav class="side-nav w-100">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link scroll" href="#home">HOME</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link scroll" href="#about-sec">ABOUT</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link scroll" href="#company-portfolio-section">PORTFOLIO</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link scroll" href="#testimonial-sec">CLIENTS</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link scroll" href="#blog-sec">BLOG</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link scroll" href="#contact-sec">CONTACT</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-12 col-lg-4 d-flex align-items-center">
+                        <div class="side-footer text-white w-100">
+                            <div class="menu-company-details">
+                                <span>+1 631 123 4567</span>
+                                <span>email@website.com</span>
+                            </div>
+                            <ul class="social-icons-simple">
+                                <li><a class="facebook-text-hvr" href="javascript:void(0)"><i class="fab fa-facebook-f"></i> </a> </li>
+                                <li><a class="instagram-text-hvr" href="javascript:void(0)"><i class="fab fa-twitter"></i> </a> </li>
+                                <li><a class="instagram-text-hvr" href="javascript:void(0)"><i class="fab fa-youtube"></i> </a> </li>
+                                <li><a class="instagram-text-hvr" href="javascript:void(0)"><i class="fab fa-instagram"></i> </a> </li>
+                            </ul>
+                            <p class="text-white">&copy; 2020 MegaOne. Made With Love by Themesindustry</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a id="close_side_menu" href="javascript:void(0);"></a>
+
+</header>
+<!--Header End-->
+
+@yield('content')
+
+<!--Footer Start-->
+<footer class="footer-style-1">
+
+    <div class="container">
+        <div class="row align-items-center">
+            <!--Social-->
+            <div class="col-lg-6">
+                <div class="footer-social text-center text-lg-left ">
+                    <ul class="list-unstyled">
+                        <li><a class="wow fadeInUp" href="javascript:void(0);"><i aria-hidden="true" class="fab fa-facebook-f"></i></a></li>
+                        <li><a class="wow fadeInDown" href="javascript:void(0);"><i aria-hidden="true" class="fab fa-twitter"></i></a></li>
+                        <li><a class="wow fadeInUp" href="javascript:void(0);"><i aria-hidden="true" class="fab fa-google-plus-g"></i></a></li>
+                        <li><a class="wow fadeInDown" href="javascript:void(0);"><i aria-hidden="true" class="fab fa-linkedin-in"></i></a></li>
+                        <li><a class="wow fadeInUp" href="javascript:void(0);"><i aria-hidden="true" class="fab fa-instagram"></i></a></li>
+                        <li><a class="wow fadeInDown" href="javascript:void(0);"><i aria-hidden="true" class="fab fa-pinterest-p"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <!--Text-->
+            <div class="col-lg-6 text-center text-lg-right">
+                <p class="company-about fadeIn">© 2020 MegaOne. Made With Love By <a href="javascript:void(0);">Themesindustry</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--Footer End-->
+
+<!-- JavaScript -->
+<script src="vendor/js/bundle.min.js"></script>
+<!-- Plugin Js -->
+<script src="vendor/js/jquery.appear.js"></script>
+<script src="vendor/js/jquery.fancybox.min.js"></script>
+<script src="vendor/js/owl.carousel.min.js"></script>
+<script src="vendor/js/parallaxie.min.js"></script>
+<script src="vendor/js/wow.min.js"></script>
+<!-- REVOLUTION JS FILES -->
+<script src="vendor/js/jquery.themepunch.tools.min.js"></script>
+<script src="vendor/js/jquery.themepunch.revolution.min.js"></script>
+<script src="vendor/js/jquery.cubeportfolio.min.js"></script>
+<!-- SLIDER REVOLUTION EXTENSIONS -->
+<script src="vendor/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="vendor/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="vendor/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="vendor/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="vendor/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="vendor/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="vendor/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="vendor/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="vendor/js/extensions/revolution.extension.video.min.js"></script>
+<!-- google map-->
+<!--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJRG4KqGVNvAPY4UcVDLcLNXMXk2ktNfY"></script>-->
+<!--<script src="creative-startup/js/map.js"></script>-->
+<!--Tilt Js-->
+<script src="creative-startup/js/slick.js"></script>
+<script src="creative-startup/js/slick.min.js"></script>
+<!-- custom script-->
+<script src="creative-startup/js/circle-progress.min.js"></script>
+
+<script src="vendor/js/contact_us.js"></script>
+<script src="creative-startup/js/script.js"></script>
+
+</body>
+</html>
