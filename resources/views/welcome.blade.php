406120e4d83856449b039fb6b68b7b7c423d8cc7
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-10-20 19:30:54 +0500

    Updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,7 @@
                 </div>
                 <div class="slider-img col-12 col-lg-6 wow fadeInRight" data-wow-delay=".8s" data-depth="0.1">
                     <div class="img-slide">
-                        <img style="width: 80px;height: 85px;" src="creative-startup/img/businesswoman-call-center-office.jpg">
+                        <img style="width: 85px;height: 90px;" src="creative-startup/img/businesswoman-call-center-office.jpg">
                     </div>
                     <div class="img-slide">
                         <img src="creative-startup/img/businessman-resume.jpg">
